47520f3ec550a1008daf8256400a2767ded4d4ad
FaikYY <yazilimcifaik@gmail.com>
2021-12-04 16:03:42 -0600

    first commit

---
+++ src/components/CardMainContainer.vue
@@ -0,0 +1,101 @@
+<!-- 
+   /$$                                       /$$             /$$              
+  | $$                                      | $$            | $$              
+ /$$$$$$    /$$$$$$  /$$$$$$/$$$$   /$$$$$$ | $$  /$$$$$$  /$$$$$$    /$$$$$$ 
+|_  $$_/   /$$__  $$| $$_  $$_  $$ /$$__  $$| $$ |____  $$|_  $$_/   /$$__  $$
+  | $$    | $$$$$$$$| $$ \ $$ \ $$| $$  \ $$| $$  /$$$$$$$  | $$    | $$$$$$$$
+  | $$ /$$| $$_____/| $$ | $$ | $$| $$  | $$| $$ /$$__  $$  | $$ /$$| $$_____/
+  |  $$$$/|  $$$$$$$| $$ | $$ | $$| $$$$$$$/| $$|  $$$$$$$  |  $$$$/|  $$$$$$$
+   \___/   \_______/|__/ |__/ |__/| $$____/ |__/ \_______/   \___/   \_______/
+                                  | $$                                        
+                                  | $$                                        
+                                  |__/                                        
+-->
+<template>
+  <div class="main-container">
+    <CardRowProfilePhoto />
+    
+    <CardRow 
+      v-bind:href="'tel:+' + '19364009468'" 
+      list_item_text="123 456 7899" 
+      icon_name="phone"
+    />
+    
+    
+    <CardRow 
+      v-bind:href="'https://www.github.com/FaikYY'" 
+      list_item_text="Github/FaikYY" 
+      icon_name="face"
+    />
+
+
+    <CardRow 
+      v-bind:href="'https://www.instagram.com/bythewayimfaik/'" 
+      list_item_text="Instagram/bythewayimfaik" 
+      icon_name="tag"
+    />
+    
+    
+    <CardRow 
+      v-bind:href="'mailto:' + 'faikyesilyaprak@outlook.com'" 
+      list_item_text="faikyesilyaprak@outlook.com" 
+      icon_name="email" 
+    />
+  
+  </div>
+</template>
+
+
+<!-- 
+                               /$$             /$$    
+                              |__/            | $$    
+  /$$$$$$$  /$$$$$$$  /$$$$$$  /$$  /$$$$$$  /$$$$$$  
+ /$$_____/ /$$_____/ /$$__  $$| $$ /$$__  $$|_  $$_/  
+|  $$$$$$ | $$      | $$  \__/| $$| $$  \ $$  | $$    
+ \____  $$| $$      | $$      | $$| $$  | $$  | $$ /$$
+ /$$$$$$$/|  $$$$$$$| $$      | $$| $$$$$$$/  |  $$$$/
+|_______/  \_______/|__/      |__/| $$____/    \___/  
+                                  | $$                
+                                  | $$                
+                                  |__/                
+-->
+<script>
+import CardRow from './CardRow/CardRow.vue';
+import CardRowProfilePhoto from './CardTop/CardTopProfile.vue';
+
+export default {
+  components: { 
+    CardRow, 
+    CardRowProfilePhoto 
+    },
+  name: 'CardMainContainer',
+  props: {
+    msg: String,
+    initialCount: Number
+  }
+}
+</script>
+
+<!-- 
+             /$$               /$$          
+            | $$              | $$          
+  /$$$$$$$ /$$$$$$   /$$   /$$| $$  /$$$$$$ 
+ /$$_____/|_  $$_/  | $$  | $$| $$ /$$__  $$
+|  $$$$$$   | $$    | $$  | $$| $$| $$$$$$$$
+ \____  $$  | $$ /$$| $$  | $$| $$| $$_____/
+ /$$$$$$$/  |  $$$$/|  $$$$$$$| $$|  $$$$$$$
+|_______/    \___/   \____  $$|__/ \_______/
+                     /$$  | $$              
+                    |  $$$$$$/              
+                     \______/               
+-->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-container{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/CardRow/CardRow.vue
@@ -0,0 +1,130 @@
+<!-- 
+   /$$                                       /$$             /$$              
+  | $$                                      | $$            | $$              
+ /$$$$$$    /$$$$$$  /$$$$$$/$$$$   /$$$$$$ | $$  /$$$$$$  /$$$$$$    /$$$$$$ 
+|_  $$_/   /$$__  $$| $$_  $$_  $$ /$$__  $$| $$ |____  $$|_  $$_/   /$$__  $$
+  | $$    | $$$$$$$$| $$ \ $$ \ $$| $$  \ $$| $$  /$$$$$$$  | $$    | $$$$$$$$
+  | $$ /$$| $$_____/| $$ | $$ | $$| $$  | $$| $$ /$$__  $$  | $$ /$$| $$_____/
+  |  $$$$/|  $$$$$$$| $$ | $$ | $$| $$$$$$$/| $$|  $$$$$$$  |  $$$$/|  $$$$$$$
+   \___/   \_______/|__/ |__/ |__/| $$____/ |__/ \_______/   \___/   \_______/
+                                  | $$                                        
+                                  | $$                                        
+                                  |__/                                        
+-->
+<template>
+  <link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet"
+  />
+
+  <a class="list-item" v-bind:href="this.href">
+    <span id="list-item-icon" class="material-icons">{{ icon_name }}</span>
+    <a id="list-item-link">{{ list_item_text }}</a>
+  </a>
+</template>
+
+<!-- 
+                               /$$             /$$    
+                              |__/            | $$    
+  /$$$$$$$  /$$$$$$$  /$$$$$$  /$$  /$$$$$$  /$$$$$$  
+ /$$_____/ /$$_____/ /$$__  $$| $$ /$$__  $$|_  $$_/  
+|  $$$$$$ | $$      | $$  \__/| $$| $$  \ $$  | $$    
+ \____  $$| $$      | $$      | $$| $$  | $$  | $$ /$$
+ /$$$$$$$/|  $$$$$$$| $$      | $$| $$$$$$$/  |  $$$$/
+|_______/  \_______/|__/      |__/| $$____/    \___/  
+                                  | $$                
+                                  | $$                
+                                  |__/                
+-->
+<script>
+export default {
+  props: {
+    href: String,
+    list_item_text: {
+      type: String,
+      default: "I am the default text",
+    },
+    icon_name: String,
+  },
+};
+</script>
+
+<!-- 
+             /$$               /$$          
+            | $$              | $$          
+  /$$$$$$$ /$$$$$$   /$$   /$$| $$  /$$$$$$ 
+ /$$_____/|_  $$_/  | $$  | $$| $$ /$$__  $$
+|  $$$$$$   | $$    | $$  | $$| $$| $$$$$$$$
+ \____  $$  | $$ /$$| $$  | $$| $$| $$_____/
+ /$$$$$$$/  |  $$$$/|  $$$$$$$| $$|  $$$$$$$
+|_______/    \___/   \____  $$|__/ \_______/
+                     /$$  | $$              
+                    |  $$$$$$/              
+                     \______/               
+-->
+<style scoped>
+* {
+  --list-item-color: rgb(100, 100, 100);
+  --list-item-color-on-hover: rgb(235, 235, 235);
+  --list-item-transition: 0.15s ease-in-out;
+}
+
+.list-item {
+  margin-top: 1.25rem;
+  width: 30rem;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+
+  padding: 1rem;
+  border-radius: 5px;
+
+  background-color: rgb(220, 220, 220);
+  transition: var(--list-item-transition);
+}
+
+#list-item-link {
+  font-size: 1.5rem;
+  color: var(--list-item-color);
+  text-decoration: none;
+
+  transition: var(--list-item-transition);
+}
+
+#list-item-icon {
+  color: var(--list-item-color);
+  font-size: 2rem;
+  padding-right: 1rem;
+
+  transition: var(--list-item-transition);
+}
+
+.list-item:hover {
+  background-color: rgb(160, 160, 160);
+}
+
+.list-item:hover #list-item-link,
+.list-item:hover #list-item-icon {
+  color: var(--list-item-color-on-hover);
+}
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+  .list-item{
+    margin-top: 1.5rem;
+    width: 20rem;
+  }
+  #list-item-link{
+    font-size: 1.1rem;
+  }
+
+  #list-item-icon{
+    font-size: 1.5rem;
+  }
+}
+</style>
+
+/* DEVELOPER NOTES: # I have used asci art as the comments # Learned v-bind, how
+to use props to change href ( any attribute of an HTML ) element # Scoped to
+make style spesific per component # Use '*{}' to be able to use CSS variables
+while using scope in vuejs */
